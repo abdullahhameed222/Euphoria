d0ea33c58d1774627d57758c1be9b3ebf9a5974a
Abdullah Hameed <abdulllahhameed123@gmail.com>
2024-08-27 01:45:58 +0500

    updated home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   const hamburger = document.querySelector('.hamburger');
-  const nav = document.querySelector('.header-nav'); // Select the navigation element
+  const nav = document.querySelector('.header__nav'); // Select the navigation element
 
   hamburger.addEventListener('click', function() {
-    this.classList.toggle('is-active'); // Toggle 'is-active' class on the hamburger
-    nav.classList.toggle('header-nav-open'); // Toggle 'nav-open' class on the navigation
+    this.classList.toggle('is-active');
+    nav.classList.toggle('heade__nav-open'); // Toggle 'nav-open' class on the navigation
   });
 });
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,17 @@
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-preset-env')({stage: 1}),
-    require('postcss-nested'),
-    require('cssnano'),
-  ]
+// module.exports = {
+//   plugins: [
+//     require('postcss-import'),
+//     require('postcss-preset-env')({stage: 1}),
+//     // require('postcss-nested'),
+//     // require('cssnano'),
+//   ]
+// }
+
+
+export default {
+  plugins: {
+    'postcss-import' :{},
+    'postcss-nested': {},
+    'postcss-preset-env': {stage: 1}
+  },
 }
